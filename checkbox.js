ad4a2a89087aab3e74c7fed55b9662c8b1aadde2
Amelie <amsc15ab@student.cbs.dk>
2019-03-22 15:04:52 +0100

    Update checkbox.js

--- checkbox.js
+++ checkbox.js
@@ -1,4 +1,4 @@
-function onlyOneCheckBox () {
+function checkBox () {
     var bachelor = document.getElementById("BA").checked;
     var master = document.getElementById("MA").checked;
         if (bachelor == false && master == false)
@@ -7,23 +7,48 @@ function onlyOneCheckBox () {
            return false; 
         }
     
-        if (bachelor == true && master == true)
+       else if (bachelor == true && master == true)
         {
             alert("Please just check one box");
             return false; 
         }
+    
+
+        if ((document.getElementById("BA").checked == true) && (document.getElementById("MA").checked == false)) {
+            window.location='file:///Users/oliverschwall/Documents/housingmatch/BA.html';
+            return false;
+        }
+    
+
+        else if ((document.getElementById("BA").checked == false) && (document.getElementById("MA").checked == true)) {
+            window.location='file:///Users/oliverschwall/Documents/housingmatch/MA.html';
+            return false;
+        }
     }
+       
+    
 
 
 
 
+// // only if one box is checked it should redirect to two more index.html sides
+// // one for BA
+// // one for MA
 
+// function checkBA(){
+// if (document.getElementById("BA").checked) {
+//         window.location='file:///Users/oliverschwall/Documents/housingmatch/BA.html';
+//         return false;
+//     }
+//     return true;
 
+// }
 
-        
-
-
-
-
-
-
+// function checkMA(){
+//     if (document.getElementById("MA").checked) {
+//             window.location='file:///Users/oliverschwall/Documents/housingmatch/MA.html';
+//             return false;
+//         }
+//         return true;
+    
+//     }
