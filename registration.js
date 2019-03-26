078cf919278d22f793130490816171e7965de576
Emil Elling <emil@bestsport.dk>
2019-03-26 11:46:40 +0100

    Update registration.js

--- registration.js
+++ registration.js
@@ -83,41 +83,39 @@ var username = usernameUI.value
 var password = passwordUI.value
 
 submitUI.onclick = function () {
-  if(firstNameUI.value.length === 0 || lastNameUI.value.length === 0 || usernameUI.value.length === 0 || passwordUI.value.length === 0) {
-  alert ("You need to fill out the form");
-  return false;
-  }
-  if(passwordUI.value.length < 5) {
-     alert ("Your password is too short");
-     return false;
-    }
-    else {
-      alert ("You need to fill out the form")
-      return false
-    }
-  
-  users.push({firstName: firstname, lastName: lastname, username: username, password: password})
-  document.getElementById('checkbox').style = 'display: none;';
-  document.getElementById('moodpictures').style = '';
-}
+ if(firstNameUI.value.length === 0 || lastNameUI.value.length === 0 || usernameUI.value.length === 0 || passwordUI.value.length === 0) {
+ alert ("You need to fill out the form");
+ return false;
+ }
+ if(passwordUI.value.length < 5) {
+    alert ("Your password is too short");
+    return false;
+   }
 
-  if (eMailUI.value.endsWith("@student.cbs.dk")){
+ if (eMailUI.value.endsWith("@student.cbs.dk")){
 
-      users.push({firstName: firstName, lastName: lastName, username: username, password: password})
-    //users.push(new User(firstName, lastName, username, password))
-    // class with  different propoerties assigned
-    // class firstName: lastName:  username:  password: 
-      console.log(users);
+     users.push({firstName: firstName, lastName: lastName, username: username, password: password})
+   //users.push(new User(firstName, lastName, username, password))
+   // class with  different propoerties assigned
+   // class firstName: lastName:  username:  password:
+     console.log(users);
+    
+   }
+ else {
+   // firstNameUI.value ="";
+   // lastNameUI.value = "";
+   usernameUI.value = "";
+   passwordUI.value = "";
+   alert ("Please enter a valid CBS email");
+   }
 
-      window.location='./match.html';
-    } 
-  else {
-    // firstNameUI.value ="";
-    // lastNameUI.value = "";
-    usernameUI.value = "";
-    passwordUI.value = "";
-    alert ("Please enter a valid CBS email");
-    }
+   window.localStorage.setItem("users", JSON.stringify(users));
+   JSON.parse(window.localStorage.getItem("users"));
+
+ }
+
+
+
 
 const images = document.getElementsByClassName('image')
 for (image of images) {
