97fc3c0c978c5250a8630b0e061ec1883a62f3dc
Tilla Lonnum <tilla.loennum@gmail.com>
2019-03-29 14:30:15 +0100

    k

---
+++ HostClasses.js
@@ -0,0 +1,19 @@
+
+class Host{
+    constructor(firstName,lastName, eMail, username,password,price,location, imagechoiceFirst, imagechoiceSecond) {
+        this.firstName= firstName; 
+        this.lastName= lastName; 
+        this.eMail = eMail; 
+        this.username = username; 
+        this.password = password; 
+        this.price =price; 
+        this.location = location; 
+        this.imagechoiceFirst = imagechoiceFirst;
+        this.imagechoiceSecond = imagechoiceSecond; 
+        
+    }
+}
+
+createString() {
+    return this.firstName+ "" + 
+}
