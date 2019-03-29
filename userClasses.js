c585bf1b53e37dcca124bdb7f3a7eb3286a00dbe
Amelie <amsc15ab@student.cbs.dk>
2019-03-29 14:19:53 +0100

    Create userClasses.js

---
+++ userClasses.js
@@ -0,0 +1,15 @@
+class Users {
+    constructor (firstName, lastName, eMail, username, password, levelofstudy, imagechoiceFirst,  imagechoiceSecond) {
+    this.firstName = firstName;
+    this.lastName =lastName;
+    this.eMail = eMail;
+     this.username =username;
+    this.password =password;
+    this.levelofstudy = levelofstudy;
+    this.imagechoiceFirst = imagechoiceFirst;
+    this.imagechoiceSecond =imagechoiceSecond;
+    }
+
+
+
+}
