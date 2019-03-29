3ecae4dadd4a444af2de1891b417151838edd66c
Amelie <amsc15ab@student.cbs.dk>
2019-03-29 14:30:05 +0100

    Update userClasses.js

--- userClasses.js
+++ userClasses.js
@@ -3,13 +3,16 @@ class Users {
     this.firstName = firstName;
     this.lastName =lastName;
     this.eMail = eMail;
-     this.username =username;
+    this.username =username;
     this.password =password;
     this.levelofstudy = levelofstudy;
     this.imagechoiceFirst = imagechoiceFirst;
     this.imagechoiceSecond =imagechoiceSecond;
     }
 
-
+    createString () {
+    return this.firstName + "" + this.lastName 
+    }
 
 }
+
