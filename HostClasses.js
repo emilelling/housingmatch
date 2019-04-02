ca5ce0431865b332c68a54c5016b9a860f9298c9
Amelie <amsc15ab@student.cbs.dk>
2019-04-02 15:05:48 +0200

    Update HostClasses.js
    corrected the classes for host and deleted the  hard coded Host file

--- HostClasses.js
+++ HostClasses.js
@@ -1,6 +1,5 @@
-
 class Host{
-    constructor(firstName,lastName, eMail, username,password,price,location, imagechoiceFirst, imagechoiceSecond) {
+    constructor(firstName,lastName, eMail, username, password, price, location, levelofstudy, imagechoiceFirst, imagechoiceSecond) {
         this.firstName= firstName; 
         this.lastName= lastName; 
         this.eMail = eMail; 
@@ -8,12 +7,26 @@ class Host{
         this.password = password; 
         this.price =price; 
         this.location = location; 
+        this.levelofstudy = levelofstudy;
         this.imagechoiceFirst = imagechoiceFirst;
         this.imagechoiceSecond = imagechoiceSecond; 
         
     }
    
-};
+}
+
+var hosts=[]
+
+hosts.push (new Host) (
+    ("Victoria", "Baker", "victoriabaker@student.cbs.dk","Viccy", "victoria123", "75000 DKK","Indre By", "BA", "1", "4"),
+    ("Fredrik", "Hansen","fredrikhansen@student.cbs.dk","Fred","hansenfredrik", "4300 DKK", "Nørrebro", "MA","2", "4"), 
+    ("Phillip", "Thompsen", "phillip.th@student.cbs.dk", "Phillip-Th", "thompsen123", "5450 DKK", "Amager","MA", "1", "3"),
+    ("Louise", "Lang", "louiselang@student.cbs.dk", "Lang1", "lang123", "6100 DKK", "Fredriksberg", "BA", "2", "3"),
+    ("David", "Sørensen", "davidsørsensen@student.cbs.dk", "DavidS", "davidsørensen", "6700 DKK", "Vestrebro", "BA", "1", "4"),
+    ("Sophie ", "Herman", "sophieherman.cbs.dk", "Sophie.herman", "Sophieherman123", "3800 DKK", "Nørrebro", "MA", "2", "4"),
+    ("Tyco", "Beck", "tycobeck@student.cbs.dk", "Tyco", "Beck123", "6000 DKK", "Indreby", "MA", "1", "3"), 
+    ("Lucy", "Watson", "lucyWatson@student.cbs.dk", "Lucywatson", "watson23","4000 DKK", "Østerbro", "BA", "2", "3")
+)
 
 
 
